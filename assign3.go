72bae909b6fcddb7bdce0e86ff6620302a9fb6fa
Salih Saad <ss17039@auis.edu.krd>
2021-04-20 01:55:22 +0300

    Req1,2 done and tested

---
+++ assign3.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var n int
+var head *object
+
+type object struct {
+	name string
+	next *object
+}
+
+func main() {
+	fmt.Println("Hello, playground")
+	getnames()
+	print()
+}
+
+func getnames() {
+	fmt.Println("Enter the number of names: ")
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		var name string
+		fmt.Println(" Enter name Number ", i+1, "  : ")
+		fmt.Scan(&name)
+		storeNames(name)
+	}
+}
+
+func storeNames(nam string) {
+
+	// create an object
+	n := object{name: nam, next: nil}
+
+	// check if it is the first name:
+	if head == nil {
+		head = &n
+	} else {
+		c := head
+		for c.next != nil {
+			c = c.next
+		}
+		c.next = &n
+	}
+
+}
+
+func print() {
+	c := head
+
+	for c != nil {
+		fmt.Println(c.name)
+		c = c.next
+	}
+}
+
+func hash(n *object) int {
+	//https://stackoverflow.com/questions/18130859/how-can-i-iterate-over-a-string-by-runes-in-go
+	name := strings.ToUpper(n.name)
+	key := 0
+	for index, char := range name {
+		if index == len(name)-2 { // if it is the last-second character
+			println(char, index)
+			key = int(char) // convert to asci code
+			break
+		}
+
+	}
+	return key - 101 // because A = 101
+}
